4606a7c835a5368791cca4c5c8e6cd9003750b76
lxj-1996 <lxj1996>
2026-08-12 00:10:34 +0800

    first

---
+++ js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?561615251e6afb0269f617de4fe6a6ff";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
